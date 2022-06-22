4095ee8cf439db668449f3d3e84583ded847b3e7
James Slagle <jslagle@redhat.com>
2022-06-22 06:49:31 -0400

    Update container tag and RHEL images for OSP 17
    - Update stack-outputs script to consolidate connection message
    - Read $ENVIRONMENT from $SCRIPT_DIR
    
    Signed-off-by: James Slagle <jslagle@redhat.com>

--- stack-create.sh
+++ stack-create.sh
@@ -2,14 +2,14 @@
 
 set -eux
 
-ENVIRONMENT_NAME=${ENVIRONMENT_NAME:-"osp17.yaml"}
+ENVIRONMENT_NAME=${ENVIRONMENT_NAME:-"osp17"}
 STACK_NAME=${STACK_NAME:-"osp17"}
 SCRIPT_DIR=$(realpath $(dirname $0))
 SSH_KEY=${SSH_KEY:-"$HOME/.ssh/${STACK_NAME}"}
 
 if [ -z "$ENVIRONMENT_NAME" ]; then
-    echo "Must provide environment file as first positional argument"
-    echo "(e.g., master.yaml, osp17.yaml)"
+    echo "Must provide release as first positional argument"
+    echo "Supported options: master, osp17"
     exit 1
 fi
 
@@ -21,11 +21,9 @@ openstack stack create ${STACK_NAME} -t ${SCRIPT_DIR}/tripleo.yaml --wait \
     --parameter-file PrivateKeyContents=${SSH_KEY} \
     --parameter-file PublicKeyContents=${SSH_KEY}.pub \
     --parameter PublicKey="${SSH_KEY}.pub" \
-    -e ${ENVIRONMENT_NAME} \
+    -e ${SCRIPT_DIR}/${ENVIRONMENT_NAME}.yaml \
     $@
 
 IP=$(openstack stack output show ${STACK_NAME} UndercloudFloatingIP -c output_value -f value)
 ssh-keygen -R $IP
-set +x
-echo "Connect with SSH as:"
-echo "ssh -i ${SSH_KEY} stack@${IP}"
+IP=${IP} ${SCRIPT_DIR}/stack-outputs.sh
--- stack-outputs.sh
+++ stack-outputs.sh
@@ -3,5 +3,14 @@
 set -eux
 
 STACK_NAME=${STACK_NAME:-"osp17"}
+SSH_KEY=${SSH_KEY:-"$HOME/.ssh/${STACK_NAME}"}
+IP=${IP:-""}
 
-openstack stack output show ${STACK_NAME} UndercloudFloatingIP
+if [ -z ${IP} ]; then
+    IP=$(openstack stack output show ${STACK_NAME} UndercloudFloatingIP -c output_value -f value)
+fi
+
+ssh-keygen -R $IP
+set +x
+echo "Connect with SSH as:"
+echo "ssh -i ${SSH_KEY} stack@${IP}"
